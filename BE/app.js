101b65aa695080292365d78885e4ff1ca2d2cfbf
Halcyon1902 <70284297+halcyon1902@users.noreply.github.com>
2023-03-03 00:59:53 +0700

    4.4
    hiển thị thông tin tác giả

--- BE/app.js
+++ BE/app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 var bodyParser = require("body-parser"); //bodyParser trả về một function hoạt động như một middleware. Chức năng lắng nghe trên req.on (\'data\') và xây dựng req.body từ các đoạn dữ liệu mà nó nhận được.
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
 dotenv.config();
 
 const tacgiaRoute = require("./routes/TacGia");
@@ -28,13 +29,12 @@ app.use(
 app.use(cors()); //CORS là một cơ chế cho phép nhiều tài nguyên khác nhau (fonts, Javascript, v.v…) của một trang web có thể được truy vấn từ domain khác với domain của trang
 app.use(morgan("common")); // khi send request sẽ thông báo dưới terminal
 //sử dụng session để kiểm tra login
-const oneDay = 1000;
 app.use(
   sessions({
-    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-    saveUninitialized: true,
-    cookie: { maxAge: oneDay },
-    resave: false,
+    secret: process.env.SERCRET_KEY,
+    saveUninitialized: false,
+    cookie: { maxAge: 900000 },
+    resave: true,
   })
 );
 //kết nối database
@@ -68,9 +68,10 @@ server.listen(8000, () => {
 app.get("/", (req, res) => {
   session = req.session;
   if (session.userid) {
-    res.render("HomePage");
+    TaiKhoan.findOne({ TaiKhoan: session.userid }, function (err, item) {
+      res.render("HomePage", { item });
+    });
   } else {
-    console.log("Chưa đăng nhập");
     res.redirect("/login");
   }
 });
@@ -151,10 +152,10 @@ app.post("/category/create", (req, res) => {
   cate.save(function (err) {
     if (err) {
       console.log("save category error:" + err);
-      return res.render("../views/category/addCategory", { message: 0 });
+      res.render("../views/category/addCategory", { message: 0 });
     } else {
       console.log("save category successfully with id category :" + cate._id);
-      return res.render("../views/category/addCategory", { message: 1 });
+      res.render("../views/category/addCategory", { message: 1 });
     }
   });
 });
@@ -163,16 +164,26 @@ app.post("/category/create", (req, res) => {
 const myusername = "admin";
 const mypassword = "admin";
 app.get("/login", (req, res) => {
-  res.render("Login");
+  //2 là bình thường
+  res.render("Login", { message: 2 });
 });
 //api đăng nhập
 app.post("/login", (req, res) => {
-  if (req.body.username == myusername && req.body.password == mypassword) {
-    session = req.session;
-    session.userid = req.body.username;
-    console.log(req.session);
-    res.redirect("/");
-  } else {
-    res.render("Login");
-  }
+  TaiKhoan.findOne({ TaiKhoan: req.body.TaiKhoan }, async function (err, item) {
+    if (!err && item != null) {
+      const valid = await bcrypt.compare(req.body.MatKhau, item.MatKhau);
+      if (valid) {
+        // đúng mật khẩu
+        session = req.session;
+        session.userid = req.body.TaiKhoan;
+        res.redirect("/");
+      } else {
+        //sai mật khẩu là 0
+        res.render("Login", { message: 0 });
+      }
+    } else {
+      //sai tài khoản là 1
+      res.render("Login", { message: 1 });
+    }
+  });
 });
